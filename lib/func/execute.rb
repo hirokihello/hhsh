a64df9f00808d423a7a26c80c01e3cc19fa3a654
hirokihello <iammyeye1@gmail.com>
2019-12-30 18:03:08 +0900

    pipe機能のテスト

--- lib/func/execute.rb
+++ lib/func/execute.rb
@@ -11,16 +11,55 @@ module Hhsh
     include Hhsh::Command
 
     def hhsh_execute(args)
-      return true if args.empty?
-      idx = 0
-      cmd = args[0]
+      # return true if args.empty?
+      # idx = 0
+      # cmd = args[0]
 
-      return false if cmd == "exit"
-      cmd_args = args.drop(1)
+      # return false if cmd == "exit"
+      # cmd_args = args.drop(1)
 
-      return launch_interpreter(args) unless BUILTIN_STR_HASH[cmd]
+      # return launch_interpreter(args) unless BUILTIN_STR_HASH[cmd]
 
-      method(BUILTIN_STR_HASH[cmd]).call(*cmd_args)
+      # method(BUILTIN_STR_HASH[cmd]).call(*cmd_args)
+
+      # child_pidは親プロセスの場合、子プロセスのpid, 子プロセスの場合はnilが代入されている
+      child_pid = fork
+      if child_pid
+        Process.waitpid(child_pid)
+      else
+        do_pipes(0)
+      end
+    end
+
+    def do_pipes(i)
+      # cat Makefile | head | grep run
+      cmd1 = ["cat", "Makefile"]
+      cmd2 = ["head"]
+      # cmd3 = ["grep", "run"]
+      # cmds = [cmd1, cmd2, cmd3]
+      cmds = [cmd1, cmd2]
+      cmds_length = cmds.length
+
+      child_pid = fork
+      r, w = IO.pipe
+      if child_pid
+        # 親
+        puts("parent before wait")
+        Process.waitpid(child_pid)
+
+        puts("parent")
+        $stdin = r
+        # exec(cmds[0].join(" "))
+        puts r
+      else
+        # 子
+        puts "child"
+        $stdout = w
+        exec(cmds[1].join(" "))
+        puts "hoge"
+        exit(true)
+      end
+      exit(true)
     end
 
     def launch_interpreter(cmd)
--- lib/main.rb
+++ lib/main.rb
@@ -5,8 +5,40 @@ module Hhsh
   class Cli
     extend Hhsh::Loop
     def self.init
-      hhsh_loop()
-      exit()
+      # hhsh_loop()
+      # exit()
+      test_pipe()
+    end
+
+    def self.test_pipe()
+      cmd1 = ["cat", "Makefile"]
+      cmd2 = ["head"]
+      cmds = [cmd1, cmd2]
+      cmds_length = cmds.length
+
+      # child_pid = fork
+      # r, w = IO.pipe
+      # if child_pid
+      #   # 親
+      #   puts("parent before wait")
+      #   Process.waitpid(child_pid)
+      #   # exec(cmds[0].join(" "))
+      #   $stdin = "Makefile"
+      #   exec("cat")
+      #   puts "cat をした"
+      #   exit(true)
+      # else
+      #   # 子
+      #   puts "child"
+      #   # $stdout = w
+      #   # exec(cmds[1].join(" "))
+      #   exit
+      # end
+      puts "hoge"
+      $stdin = "Makefile"
+      exec("cat")
+      puts "cat をした"
+      exit(true)
     end
   end
 end
