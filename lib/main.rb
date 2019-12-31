04e9c9770d9e4a7a2d0e5c2a1c133546ed725810
hirokihello <iammyeye1@gmail.com>
2019-12-31 14:03:16 +0900

    パイプ機能の再帰部分の実装

--- lib/main.rb
+++ lib/main.rb
@@ -7,38 +7,39 @@ module Hhsh
     def self.init
       # hhsh_loop()
       # exit()
-      test_pipe()
+      test_pipe(0)
     end
 
-    def self.test_pipe()
-      cmd1 = ["cat", "Makefile"]
-      cmd2 = ["head"]
-      cmds = [cmd1, cmd2]
+    def self.test_pipe(i)
+      # cat Makefile | head | grep run
+      cmd1 = "cat Makefile"
+      cmd2 = "head"
+      cmd3 = "grep run"
+      cmds = [cmd1, cmd2, cmd3]
       cmds_length = cmds.length
 
-      # child_pid = fork
-      # r, w = IO.pipe
-      # if child_pid
-      #   # 親
-      #   puts("parent before wait")
-      #   Process.waitpid(child_pid)
-      #   # exec(cmds[0].join(" "))
-      #   $stdin = "Makefile"
-      #   exec("cat")
-      #   puts "cat をした"
-      #   exit(true)
-      # else
-      #   # 子
-      #   puts "child"
-      #   # $stdout = w
-      #   # exec(cmds[1].join(" "))
-      #   exit
-      # end
-      puts "hoge"
-      $stdin = "Makefile"
-      exec("cat")
-      puts "cat をした"
-      exit(true)
+      # 再帰の回数がコマンド数に達した場合、一番左のコマンドを発火させる
+      exec(cmds[0]) if i == (cmds_length - 1)
+
+      r, w = IO.pipe()
+
+      child_pid = fork do
+        # 子
+        # 標準出力先をwに置き換え
+        $stdout.reopen(w)
+        #処理が終わったら閉じる
+        w.close()
+        self.test_pipe(i + 1)
+      end
+
+      # 親の処理
+      Process.waitpid(child_pid)
+
+      # 標準入力をパイプに置き換え
+      $stdin.reopen(r)
+      r.close()
+
+      exec(cmds[cmds_length - i - 1])
     end
   end
 end
